b7c238eb0c9d221fd8256fbd99e09dfbccc66603
Will Studabaker <will.studabaker@gmail.com>
2020-10-21 17:55:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,10 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  else
+    true
+  end
 end
