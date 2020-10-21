c42fc115fac590602958624ee02fef85a3a307ce
Will Studabaker <will.studabaker@gmail.com>
2020-10-21 17:58:53 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  pos = board[index]
+  if pos == " " || pos == "" || pos == nil
+    false
+  else
+    true
+  end
+end
