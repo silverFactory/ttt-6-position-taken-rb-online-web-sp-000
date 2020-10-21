87fd66732df61727f5fc2f64ac2f8fa1149bb02a
Will Studabaker <will.studabaker@gmail.com>
2020-10-21 17:57:57 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " "
-    false
-  elsif board[index] == ""
-    false
-  elsif board[index] == nil
+  pos = board[index]
+  if pos == " " || pos == "" || pos == nil
     false
   else
     true
